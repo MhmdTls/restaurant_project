0eb811f25c778f2e06a32e13d68af0a7ff98d2d6
MhmdTls <127507753+MhmdTls@users.noreply.github.com>
2025-10-25 23:50:25 +0200

    Fix import path for views in urls.py

--- Resturant_Project/urls.py
+++ Resturant_Project/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from Base_App.views import *
+from Resturant_Project.Base_App.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin_pannel'),
